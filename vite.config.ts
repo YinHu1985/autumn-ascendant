94ed8e6ae429eac104fc49b1a490320fd075e2ef
Yin Hu <yinhu@Yins-MacBook-Pro.local>
2026-02-17 21:24:25 +0800

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/autumn-ascendant/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
-    globals: true
-  }
+    globals: true,
+  },
 })
